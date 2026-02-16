e8c4b02e85de358f326fa25d7b145774d4814e84
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-16 19:44:01 -0300

    Criando Controller e Rota de Logs da Entrega

---
+++ src/controllers/delivery-logs-controller.ts
@@ -0,0 +1,9 @@
+import type { Request, Response } from 'express'
+
+class DeliveryLogsController {
+  async create(request: Request, response: Response) {
+    return response.json({ message: 'ok' })
+  }
+}
+
+export { DeliveryLogsController }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import { deliveriesRoutes } from './deliveries-routes'
+import { deliveryLogsRoutes } from './delivery-logs-routes'
 import { sessionsRoutes } from './sessions-routes'
 import { usersRoutes } from './users-routes'
 
@@ -8,5 +9,6 @@ const routes = Router()
 routes.use('/users', usersRoutes)
 routes.use('/sessions', sessionsRoutes)
 routes.use('/deliveries', deliveriesRoutes)
+routes.use('/delivery-logs', deliveryLogsRoutes)
 
 export { routes }
