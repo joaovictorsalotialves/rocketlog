33032dbd37737ef3d075e7a36554188796fbbcca
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-16 17:46:00 -0300

    Cadastra Entrega

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,8 +1,25 @@
 import type { Request, Response } from 'express'
+import { z } from 'zod'
+
+import { prisma } from '@/database/prisma'
 
 class DeliveriesController {
   async create(request: Request, response: Response) {
-    return response.json({ message: 'Ok' })
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string(),
+    })
+
+    const { user_id, description } = bodySchema.parse(request.body)
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    })
+
+    return response.status(201).json()
   }
 }
 
