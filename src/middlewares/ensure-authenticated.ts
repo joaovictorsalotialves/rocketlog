695e3094e3d9854e367d109bf8ce313896988e6d
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-16 17:07:45 -0300

    Criando ensure-authenticated

---
+++ src/middlewares/ensure-authenticated.ts
@@ -0,0 +1,35 @@
+import type { NextFunction, Request, Response } from 'express'
+import { verify } from 'jsonwebtoken'
+
+import { authConfig } from '@/configs/auth'
+import { AppError } from '@/utils/AppError'
+
+interface TokenPayload {
+  role: string
+  sub: string
+}
+
+function ensureAuthenticated(request: Request, _response: Response, next: NextFunction) {
+  try {
+    const authHeader = request.headers.authorization
+
+    if (!authHeader) {
+      throw new AppError('JWT token is missing', 401)
+    }
+
+    const [, token] = authHeader.split(' ')
+
+    const { role, sub: user_id } = verify(token, authConfig.jwt.secret) as TokenPayload
+
+    request.user = {
+      id: user_id,
+      role,
+    }
+
+    return next()
+  } catch {
+    throw new AppError('Invalid JWT token', 401)
+  }
+}
+
+export { ensureAuthenticated }
