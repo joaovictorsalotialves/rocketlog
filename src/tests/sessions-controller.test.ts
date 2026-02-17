2a27105ee3ca29ccc7ee2911235e7afc77f68875
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-17 01:06:05 -0300

    Testando Autenticação

---
+++ src/tests/sessions-controller.test.ts
@@ -0,0 +1,34 @@
+import request from 'supertest'
+
+import { app } from '@/app'
+import { prisma } from '@/database/prisma'
+
+describe('SessionsController', () => {
+  let user_id: string
+
+  afterAll(async () => {
+    await prisma.user.delete({
+      where: {
+        id: user_id,
+      },
+    })
+  })
+
+  it('should authenticate and get access token', async () => {
+    const userResponse = await request(app).post('/users').send({
+      name: 'Test User',
+      email: 'test@example.com',
+      password: 'password123',
+    })
+
+    user_id = userResponse.body.id
+
+    const sessionResponse = await request(app).post('/sessions').send({
+      email: 'test@example.com',
+      password: 'password123',
+    })
+
+    expect(sessionResponse.status).toBe(200)
+    expect(sessionResponse.body.token).toEqual(expect.any(String))
+  })
+})
