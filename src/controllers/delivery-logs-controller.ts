017bed75a549a8d4270dd32d7776fe56df43d56c
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-16 22:53:40 -0300

    Condição de Pedido Entregue

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -21,6 +21,10 @@ class DeliveryLogsController {
       throw new AppError('Delivery not found', 404)
     }
 
+    if (delivery.status === 'delivered') {
+      throw new AppError('This order has already been delivered, you cannot add more logs', 400)
+    }
+
     if (delivery.status === 'processing') {
       throw new AppError('Change status to shipped before adding a log', 400)
     }
