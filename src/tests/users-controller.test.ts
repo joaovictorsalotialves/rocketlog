697d54b4fa767eb3fd7e9cd7ea16e79f6f50cea0
João Victor <joaovictorsalotialves12@gmail.com>
2026-02-17 00:53:11 -0300

    Testando Cadastro de Usuário com E-mail Indisponível

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -27,4 +27,15 @@ describe('UsersController', () => {
 
     user_id = response.body.id
   })
+
+  it('should throw an error if user with same email already exists', async () => {
+    const response = await request(app).post('/users').send({
+      name: 'Duplicate User',
+      email: 'test@example.com',
+      password: 'password123',
+    })
+
+    expect(response.status).toBe(409)
+    expect(response.body.message).toBe('User with same email already exists')
+  })
 })
