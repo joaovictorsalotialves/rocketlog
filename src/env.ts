7998aa23cbeb69e0d4fcb618a84a2fee1cddc8e6
João Victor <joaovictorsalotialves12@gmail.com>
2026-03-23 11:49:47 -0300

    Configurando a porta

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   JWT_SECRET: z.string().min(1),
+  PORT: z.coerce.number().default(3333),
 })
 
 const env = envSchema.parse(process.env)
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import { app } from '@/app'
+import { env } from '@/env'
 
-const PORT = 3333
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+app.listen(env.PORT, () => {
+  console.log(`Server is running on port ${env.PORT}`)
 })
